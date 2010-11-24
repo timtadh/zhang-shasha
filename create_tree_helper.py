9cca80bf048c8206eebe906b653a937670c9fd07
Stephen Johnson <srj15@case.edu>
2010-11-24 00:01:06 -0500

    Removed unnecessary add_edge() function in favor of defaultdict

--- create_tree_helper.py
+++ create_tree_helper.py
@@ -1,3 +1,4 @@
+import collections
 import tree
 """
 This takes a String describing a tree and converts it into a
@@ -20,25 +21,13 @@ def make_tree(tree_spec, root_id=None):
 
     For example: "a-b;a-c;c-d;c-e;c-f;"
     """
-    a_tree = {}
+    a_tree = collections.defaultdict(list)
     root = root_id
     
     for edge in tree_spec.split(';'):
         nodes = edge.split('-')
-        add_edge(nodes[0], nodes[1], a_tree)
+        a_tree[nodes[0]].append(nodes[1])
+        a_tree[nodes[1]]
         root = root or nodes[0]
     
     return tree.BasicTree(a_tree, root, tree.BasicTree.POSTORDER)
-
-def add_edge(parent_label, child_label, tree_structure):
-    """This adds the edge (and nodes if necessary) to the tree
-    definition."""
-    
-    # Add Parent node, edge and child
-    if not tree_structure.has_key(parent_label):
-        tree_structure[parent_label] = []
-    tree_structure[parent_label].append(child_label)
-
-    # Add child if not already there
-    if not tree_structure.has_key(child_label):
-        tree_structure[child_label] = []
