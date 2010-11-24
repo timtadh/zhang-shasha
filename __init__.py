30f102f8fe1049446f01814ef978942f8db9eef6
Stephen Johnson <srj15@case.edu>
2010-11-23 23:56:30 -0500

    Removed pointless object wrapper for distance function

--- __init__.py
+++ __init__.py
@@ -3,8 +3,7 @@ from create_tree_helper import make_tree
 from comparison_zhang_shasha import *
 
 def distance(a, b):
-    tree_corrector = ComparisonZhangShasha()
-    return tree_corrector.find_distance(make_tree(a), make_tree(b))
+    return find_distance(make_tree(a), make_tree(b))
 
 if __name__ == '__main__':
     assert distance('a-b;a-c', 'a-b;a-d') == 1
--- comparison_zhang_shasha.py
+++ comparison_zhang_shasha.py
@@ -33,115 +33,100 @@ def default_replace_cost_func(a_node_id, b_node_id, a_tree, b_tree):
     else:
         return 1
 
-class ComparisonZhangShasha(object):
+def find_distance(a_tree, b_tree, ops=None):
     """
-    "Dynamic Programming" Table.
-    use function setFD to access it.
-    Each call to findDistance will change these tables.  But each
-    call is independent (and reinitialises this) so the side effect
-    has no real consequence.  ie.  There are NO public side effects.
+    This is initialised to be n+1 * m+1.  It should really be n*m
+    but because of java's zero indexing, the for loops would
+    look much more readable if the matrix is extended by one
+    column and row.  So, distance[0,*] and distance[*,0] should
+    be permanently zero.
     """
+    ops = ops or {
+        INSERT: lambda *args: 1,
+        DELETE: lambda *args: 1,
+        RENAME: default_replace_cost_func
+    }
     
-    def __init__(self):
-        super(ComparisonZhangShasha, self).__init__()
-        self.forest_distance = None
-        self.distance = None
+    distance = multidim_arr(len(a_tree)+1, len(b_tree)+1)
+
+    # Preliminaries
+    #      1. Find left-most leaf and key roots
+    a_left_leaf = {}
+    b_left_leaf = {}
+    a_tree_key_roots = []
+    b_tree_key_roots = []
     
-    def find_distance(self, a_tree, b_tree, ops=None):
-        """
-        This is initialised to be n+1 * m+1.  It should really be n*m
-        but because of java's zero indexing, the for loops would
-        look much more readable if the matrix is extended by one
-        column and row.  So, distance[0,*] and distance[*,0] should
-        be permanently zero.
-        """
-        ops = ops or {
-            INSERT: lambda *args: 1,
-            DELETE: lambda *args: 1,
-            RENAME: default_replace_cost_func
-        }
-        
-        self.distance = multidim_arr(len(a_tree)+1, len(b_tree)+1)
+    find_helper_tables(a_tree, a_left_leaf, a_tree_key_roots, a_tree.get_root_id())
+    find_helper_tables(b_tree, b_left_leaf, b_tree_key_roots, b_tree.get_root_id())
+    
+    # Comparison
+    for a_key_root in a_tree_key_roots:
+        for b_key_root in b_tree_key_roots:
+            # Re-initialise forest distance tables
+            fD = collections.defaultdict(lambda: collections.defaultdict(lambda: 0.0))
+            
+            fD[a_left_leaf[a_key_root]][b_left_leaf[b_key_root]] = 0.0
+            
+            # for all descendents of aKeyroot: i
+            for i in xrange(a_left_leaf[a_key_root], a_key_root+1):
+                new_val = fD[i-1][b_left_leaf[b_key_root]-1] + \
+                          ops[DELETE](i, 0, a_tree, b_tree)
+                fD[i][b_left_leaf[b_key_root]-1] = new_val
 
-        # Preliminaries
-        #      1. Find left-most leaf and key roots
-        a_left_leaf = {}
-        b_left_leaf = {}
-        a_tree_key_roots = []
-        b_tree_key_roots = []
-        
-        self.find_helper_tables(a_tree, a_left_leaf, a_tree_key_roots, a_tree.get_root_id())
-        self.find_helper_tables(b_tree, b_left_leaf, b_tree_key_roots, b_tree.get_root_id())
-        
-        # Comparison
-        for a_key_root in a_tree_key_roots:
-            for b_key_root in b_tree_key_roots:
-                # Re-initialise forest distance tables
-                fD = collections.defaultdict(lambda: collections.defaultdict(lambda: 0.0))
-                
-                fD[a_left_leaf[a_key_root]][b_left_leaf[b_key_root]] = 0.0
-                
-                # for all descendents of aKeyroot: i
-                for i in xrange(a_left_leaf[a_key_root], a_key_root+1):
-                    new_val = fD[i-1][b_left_leaf[b_key_root]-1] + \
-                              ops[DELETE](i, 0, a_tree, b_tree)
-                    fD[i][b_left_leaf[b_key_root]-1] = new_val
-
-                # for all descendents of bKeyroot: j
+            # for all descendents of bKeyroot: j
+            for j in xrange(b_left_leaf[b_key_root], b_key_root+1):
+                new_val = fD[a_left_leaf[a_key_root]-1][j-1] + \
+                          ops[INSERT](0, j, a_tree, b_tree)
+                fD[a_left_leaf[a_key_root]-1][j] = new_val
+            
+            # for all descendents of aKeyroot: i
+            for i in xrange(a_left_leaf[a_key_root], a_key_root+1):
                 for j in xrange(b_left_leaf[b_key_root], b_key_root+1):
-                    new_val = fD[a_left_leaf[a_key_root]-1][j-1] + \
-                              ops[INSERT](0, j, a_tree, b_tree)
-                    fD[a_left_leaf[a_key_root]-1][j] = new_val
-                
-                # for all descendents of aKeyroot: i
-                for i in xrange(a_left_leaf[a_key_root], a_key_root+1):
-                    for j in xrange(b_left_leaf[b_key_root], b_key_root+1):
-                        # This min compares del vs ins
-                        minimum = min(
-                            # Option 1: Delete node from a_tree
-                            fD[i-1][j] + ops[DELETE](i, 0, a_tree, b_tree),
-                            # Option 2: Insert node into b_tree
-                            fD[i][j-1] + ops[INSERT](0, j, a_tree, b_tree)
+                    # This min compares del vs ins
+                    minimum = min(
+                        # Option 1: Delete node from a_tree
+                        fD[i-1][j] + ops[DELETE](i, 0, a_tree, b_tree),
+                        # Option 2: Insert node into b_tree
+                        fD[i][j-1] + ops[INSERT](0, j, a_tree, b_tree)
+                    )
+                    
+                    if a_left_leaf[i] == a_left_leaf[a_key_root] and \
+                    b_left_leaf[j] == b_left_leaf[b_key_root]:
+                        distance[i][j] = min(
+                            minimum,
+                            fD[i-1][j-1] + ops[RENAME](i, j, a_tree,b_tree)
                         )
-                        
-                        if a_left_leaf[i] == a_left_leaf[a_key_root] and \
-                        b_left_leaf[j] == b_left_leaf[b_key_root]:
-                            self.distance[i][j] = min(
-                                minimum,
-                                fD[i-1][j-1] + ops[RENAME](i, j, a_tree,b_tree)
-                            )
-                            fD[i][j] = self.distance[i][j]
-                        else:
-                            fD[i][j] = min(minimum, fD[a_left_leaf[i]-1][b_left_leaf[j]])
-        return self.distance[len(a_tree)][len(b_tree)]
+                        fD[i][j] = distance[i][j]
+                    else:
+                        fD[i][j] = min(minimum, fD[a_left_leaf[i]-1][b_left_leaf[j]])
+    return distance[len(a_tree)][len(b_tree)]
 
-    def find_helper_tables(self, some_tree, leftmost_leaves, keyroots, a_node_id):
-        """
-        The initiating call should be to the root node of the tree.
-        It fills in an nxn (hash) table of the leftmost leaf for a
-        given node.  It also compiles an array of key roots. The
-        integer values IDs must come from the post-ordering of the
-        nodes in the tree.
-        """
-        self.find_helper_tables_recurse(some_tree, leftmost_leaves, keyroots, a_node_id)
+def find_helper_tables(some_tree, leftmost_leaves, keyroots, a_node_id):
+    """
+    The initiating call should be to the root node of the tree.
+    It fills in an nxn (hash) table of the leftmost leaf for a
+    given node.  It also compiles an array of key roots. The
+    integer values IDs must come from the post-ordering of the
+    nodes in the tree.
+    """
+    find_helper_tables_recurse(some_tree, leftmost_leaves, keyroots, a_node_id)
 
-        # add root to keyroots
-        keyroots.append(a_node_id)
+    # add root to keyroots
+    keyroots.append(a_node_id)
 
-        # add boundary nodes
-        leftmost_leaves[0] = 0
-    
-    def find_helper_tables_recurse(self, some_tree, leftmost_leaves, keyroots, a_node_id):
-        # If this is a leaf, then it is the leftmost leaf
-        if some_tree.is_leaf(a_node_id):
-            leftmost_leaves[a_node_id] = a_node_id
-        else:
-            seen_leftmost = False
-            for child in some_tree.get_children_ids(a_node_id):
-                self.find_helper_tables_recurse(some_tree, leftmost_leaves, keyroots, child)
-                if not seen_leftmost:
-                    leftmost_leaves[a_node_id] = leftmost_leaves[child]
-                    seen_leftmost = True
-                else:
-                    keyroots.append(child)
-    
+    # add boundary nodes
+    leftmost_leaves[0] = 0
+
+def find_helper_tables_recurse(some_tree, leftmost_leaves, keyroots, a_node_id):
+    # If this is a leaf, then it is the leftmost leaf
+    if some_tree.is_leaf(a_node_id):
+        leftmost_leaves[a_node_id] = a_node_id
+    else:
+        seen_leftmost = False
+        for child in some_tree.get_children_ids(a_node_id):
+            find_helper_tables_recurse(some_tree, leftmost_leaves, keyroots, child)
+            if not seen_leftmost:
+                leftmost_leaves[a_node_id] = leftmost_leaves[child]
+                seen_leftmost = True
+            else:
+                keyroots.append(child)
